16c71223fd3131d084be193a6e87580ffa9e55e6
Anguel Sirlopu C <design.anguel@gmail.com>
2017-07-12 13:47:30 -0500

    Init Responsive

--- js/anguelsc.js
+++ js/anguelsc.js
@@ -1,5 +1,6 @@
 var me = Snap('#me');
-Snap.load('icons/me.svg', onSVGLoaded ) ;
+
+Snap.load('http://localhost/v1/icons/me.svg', onSVGLoaded ) ;
 
 function onSVGLoaded( data ){ 
     me.append( data );
